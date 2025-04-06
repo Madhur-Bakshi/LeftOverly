f308d7076c07b1d9b317a1c3e9bd43535c11949c
Madhur <sharma15madhur@gmail.com>
2025-04-06 17:27:50 +0530

    project initialisation

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import RecipeDetail from './pages/RecipeDetail'
+import RecipeResults from './pages/RecipeResults'
+import Login from './components/Login'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<RecipeResults />} />
+        <Route path="/recipes/:id" element={<RecipeDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
